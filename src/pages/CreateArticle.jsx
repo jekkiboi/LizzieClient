7ae9a20ee57c71903117fa634709de6467dadee7
jekkiboi <hollymayer4@gmail.com>
2021-11-14 12:47:31 -0800

    send

--- src/pages/CreateArticle.jsx
+++ src/pages/CreateArticle.jsx
@@ -1,5 +1,5 @@
 //UPLOAD CARE API KEY= `4a022e7b929332d401c9`
-// import { render } from '@testing-library/react'
+
 import React from 'react'
 import { useState } from "react"
 import { useAuth0 } from '@auth0/auth0-react'
